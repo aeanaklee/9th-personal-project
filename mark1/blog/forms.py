84e9562fe8ad2e19ef89122b7fc75d9e1aabfcb7
aeanaklee <2juhee99@gmail.com>
2021-09-23 15:09:21 +0900

    firts commit

---
+++ mark1/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.db import models
+from django.forms import fields
+from .models import Blog, Comment, Category
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:  # 일종의 이름표 역할
+        model = Blog
+        fields = ['category', 'title', 'body', ]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author_name', 'comment_text')
---
+++ mark1/blog/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        unique_together = ["category_name"]
+
+    def __str__(self):
+        return self.category_name
+
+
+class HashTag(models.Model):
+    hashtag_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hashtag_name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+    hashtag = models.ManyToManyField(HashTag)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Blog, related_name="comments", on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def approve(self):
+        self.save()
+
+    def __str__(self):
+        return self.comment_text
